28dac7405257040e76ca98c1b4f3dd7d6be07d6f
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-11-08 21:10:01 -0300

    add: Faker dependency and create utils with utility class FakerUser

---
+++ src/main/java/com/codo/tpgrupo/springpractices/utils/FakeUsers.java
@@ -0,0 +1,30 @@
+package com.codo.tpgrupo.springpractices.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.codo.tpgrupo.springpractices.models.User;
+
+import lombok.experimental.UtilityClass;
+import net.datafaker.Faker;
+
+@UtilityClass
+public class FakeUsers {
+    private Faker faker = new Faker();
+
+    public User generateUser(){
+        return new User(
+            faker.name().firstName(),
+            faker.name().lastName(),
+            faker.number().numberBetween(12, 80)
+        ); 
+    }
+
+    public static List<User> generateUsers(int quantity){
+        List<User> users = new ArrayList<>();
+        for(int i=0; i<quantity; i++){
+            users.add(generateUser());
+        }
+        return users;
+    }
+}
