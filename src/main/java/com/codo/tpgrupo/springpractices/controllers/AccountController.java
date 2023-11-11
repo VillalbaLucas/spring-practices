101b5ea3e216123e713153c2e66baf144e58fa57
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-11-11 13:25:17 -0300

    create: POST method con User id with parameter
    es un metodo post en AccountService para crear una cuenta con un id de usuario existente

--- src/main/java/com/codo/tpgrupo/springpractices/controllers/AccountController.java
+++ src/main/java/com/codo/tpgrupo/springpractices/controllers/AccountController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,13 +29,19 @@ public class AccountController {
         return ResponseEntity.ok().body(service.findAll());
     }
 
-    @PostMapping("")
-    public ResponseEntity<?> saveAccount(@RequestBody Account account){
+    @PostMapping("/{id}")
+    public ResponseEntity<?> saveAccount(@PathVariable Long id, @RequestBody Account account){
+        
         try{
-           service.save(account);
+           service.save(account, id);
            return ResponseEntity.ok().body(account); 
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(e);
         }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> get(@PathVariable Long id){
+        return ResponseEntity.ok().body(id);
     }
 }
