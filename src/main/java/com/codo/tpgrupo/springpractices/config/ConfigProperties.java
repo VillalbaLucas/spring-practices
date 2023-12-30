73eb31fde9843de7c03716ccca52c0b9277bdd45
VillalbaLucas <lucasvllba.develop@gmail.com>
2023-12-30 00:19:30 -0300

    delete: unnecesary configurations

--- src/main/java/com/codo/tpgrupo/springpractices/config/ConfigProperties.java
+++
@@ -1,9 +0,0 @@
-package com.codo.tpgrupo.springpractices.config;
-
-
-// @PropertySource("my.sources")
-public class ConfigProperties {
-
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "root";
-}
